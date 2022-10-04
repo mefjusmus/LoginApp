d55ffa080b3583abc9ef9301aac3bb9c56dc09f1
Vladislav <vsuslov7@gmail.com>
2022-10-04 22:03:11 +0300

    Set gradient for greetingVC

--- LoginApp/GreetingViewController.swift
+++ LoginApp/GreetingViewController.swift
@@ -16,5 +16,30 @@ class GreetingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         greetingLabel.text = "Welcome, \(userName ?? "")"
+        setGradient()
+    }
+    
+    private func setGradient() {
+        let gradient = CAGradientLayer()
+        
+        let color1 = UIColor(
+            red: 197 / 255,
+            green: 125 / 255,
+            blue: 141 / 255,
+            alpha: 1
+        ).cgColor
+        
+        let color2 = UIColor(
+            red: 135 / 255,
+            green: 152 / 255,
+            blue: 220 / 255,
+            alpha: 1
+        ).cgColor
+        
+        gradient.colors = [color1, color2]
+        gradient.frame = view.bounds
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        view.layer.insertSublayer(gradient, at: 0)
     }
 }
---
+++ LoginApp/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  LoginApp
+//
+//  Created by Vladislav Suslov on 4.10.22.
+//
+
+import Foundation
+
+struct UserModel {
+    let userName = "User"
+    let password = "Password"
+}
